0866b3854318f13ed7ab4d45036afbbeb78ee5c2
Anderson Diana <p000adiana@prservicos.com.br>
2017-08-25 08:53:46 -0300

    Initial Commit

---
+++ WeatherLikeAD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherLikeAD
+//
+//  Created by Anderson Diana on 2017-08-25.
+//  Copyright © 2017 anderson.queiroz.diana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
