28800f3da52f6c05dec2b4ba99be63f005cb42e0
Anderson Diana <p000adiana@prservicos.com.br>
2017-08-28 15:39:12 -0300

    Criação da tableView e tableCell ( com auto-layout)

---
+++ WeatherTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  WeatherTableViewCell.h
+//  WeatherLikeAD
+//
+//  Created by Anderson Diana on 2017-08-28.
+//  Copyright © 2017 anderson.queiroz.diana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeatherTableViewCell : UITableViewCell
+
+@end
---
+++ WeatherTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  WeatherTableViewCell.m
+//  WeatherLikeAD
+//
+//  Created by Anderson Diana on 2017-08-28.
+//  Copyright © 2017 anderson.queiroz.diana. All rights reserved.
+//
+
+#import "WeatherTableViewCell.h"
+
+@implementation WeatherTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
